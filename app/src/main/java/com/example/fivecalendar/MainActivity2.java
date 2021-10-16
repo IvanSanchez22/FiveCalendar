a965e217e977779008674898629b6f608b491348
migueelruiiz <miguelruizmoran@gmail.com>
2021-10-16 17:21:49 +0200

    Commit Miguel Sabado 16

---
+++ app/src/main/java/com/example/fivecalendar/MainActivity2.java
@@ -0,0 +1,27 @@
+package com.example.fivecalendar;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    CalendarView cal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        cal = findViewById(R.id.calendarView);
+
+        cal.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String fecha = dayOfMonth + "/" + month + "/" + year ;
+                Toast.makeText(MainActivity2.this, fecha, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
