9e3ef2af1e2d1b74fb640b6e1f620ec33e97d8c0
a-sanchezgu-2019 <a.sanchezgu.2019@alumnos.urjc.es>
2021-10-16 19:45:28 +0200

    Cambios Main

--- app/src/main/java/com/example/fivecalendar/CalendarioActivity.java
+++ app/src/main/java/com/example/fivecalendar/CalendarioActivity.java
@@ -1,26 +1,31 @@
 package com.example.fivecalendar;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CalendarView;
 import android.widget.Toast;
 
-public class MainActivity2 extends AppCompatActivity {
+public class CalendarioActivity extends AppCompatActivity {
 
     CalendarView cal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_calendario);
         cal = findViewById(R.id.calendarView);
 
         cal.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 String fecha = dayOfMonth + "/" + month + "/" + year ;
-                Toast.makeText(MainActivity2.this, fecha, Toast.LENGTH_LONG).show();
+                Toast.makeText(CalendarioActivity.this, fecha, Toast.LENGTH_LONG).show();
+                /*Intent intent = new Intent(this, );
+                int date[] = {dayOfMonth, month, year};
+                intent.putExtra("date", date);
+                startActivity(intent);*/
             }
         });
     }
--- app/src/main/java/com/example/fivecalendar/MainActivity.java
+++ app/src/main/java/com/example/fivecalendar/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.fivecalendar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //tv = findViewById(R.id.textView);
+    }
+
+    public void abrirCalendario(View view) {
+
+        Intent i = new Intent(this, CalendarioActivity.class);
+        startActivity(i);
+        /*Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog dpd = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                String fecha = dayOfMonth + "/" + month + "/" + year ;
+                tv.setText(fecha);
+            }
+        }, year, month, day); dpd.getWindow().setBackgroundDrawable(new ColorDrawable());
+        dpd.show();*/
     }
 }
